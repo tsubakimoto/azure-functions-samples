0d24f1fedca56f0f2e0f883bd6c496c63253cf17
Yuta Matsumura <tkmbmbr@gmail.com>
2022-04-17 07:32:19 +0900

    Add table output sample

---
+++ table-managed-identity/SingleItemOutput.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Data.Tables;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureTableManagedIdentitySample
+{
+    public static class SingleItemOutput
+    {
+        [FunctionName("SingleItemOutput")]
+        [return: Table("Items", Connection = "MyTableService")]
+        public static TableEntity Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string key = req.Query["key"];
+            return new TableEntity("item1", key);
+        }
+    }
+}
