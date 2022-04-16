0e9f06cd0083b418c553ba5d67cfa116f5d98768
Yuta Matsumura <tkmbmbr@gmail.com>
2022-04-16 21:43:08 +0900

    Add Azure Table Managed Identity Sample

---
+++ table-managed-identity/Function1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Data.Tables;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureTableManagedIdentitySample
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Table("Items", "item1", "1", Connection = "MyTableService")] TableEntity entity,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string responseMessage = $"PK={entity.PartitionKey}, RK={entity.RowKey}, Timestamp={entity.Timestamp}";
+            return new OkObjectResult(responseMessage);
+        }
+
+        public class MyEntity : ITableEntity
+        {
+            public string PartitionKey { get; set; }
+            public string RowKey { get; set; }
+            public string Name { get; set; }
+            public DateTimeOffset? Timestamp { get; set; }
+            public ETag ETag { get; set; }
+        }
+    }
+}
---
+++ table-managed-identity/README.md
@@ -0,0 +1,2 @@
+# azure-functions-table-managed-identity-sample
+https://azure.microsoft.com/en-us/updates/public-preview-azure-tables-extension-for-azure-functions/
