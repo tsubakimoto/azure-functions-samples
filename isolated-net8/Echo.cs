fe7a1a7fc122b3aaa54fa7352c27395f85e7a457
Yuta Matsumura <tkmbmbr@gmail.com>
2023-09-17 15:25:59 +0900

    add function with openapi

---
+++ isolated-net8/Echo.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace isolated_net8;
+
+public class Echo
+{
+    private readonly ILogger _logger;
+
+    public Echo(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<Echo>();
+    }
+
+    [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+    [Function("Echo")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+    }
+}
--- isolated-net8/Program.cs
+++ isolated-net8/Program.cs
@@ -1,7 +1,10 @@
+﻿// https://github.com/Azure/azure-functions-openapi-extension/blob/main/docs/enable-open-api-endpoints-out-of-proc.md
+
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWorkerDefaults(worker => worker.UseNewtonsoftJson())
     .Build();
 
 host.Run();
