ce3034901082e36ab5113f98ae710e524bcc420f
Yuta Matsumura <tkmbmbr@gmail.com>
2024-12-30 22:11:10 +0900

    add semantic kernel

---
+++ isolated-net9-semantic-kernel/Function1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace isolated_net9_semantic_kernel;
+
+public class Function1
+{
+    private readonly ILogger<Function1> _logger;
+    private readonly IChatCompletionService _chatService;
+
+    public Function1(ILogger<Function1> logger, IChatCompletionService chatService)
+    {
+        _logger = logger;
+        _chatService = chatService;
+    }
+
+    [Function("Function1")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+    {
+        var prompt = "Microsoft Azureとはなんですか？";
+        var completion = await _chatService.GetChatMessageContentAsync(prompt);
+        return new OkObjectResult(completion);
+    }
+}
---
+++ isolated-net9-semantic-kernel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ClientModel;
+using Azure.AI.OpenAI;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();
+
+var configuration = builder.Configuration;
+
+builder.Services.AddAzureOpenAIChatCompletion(
+    configuration["AzureOpenAI:DeploymentName"],
+    new AzureOpenAIClient(
+        new Uri(configuration["AzureOpenAI:Endpoint"]),
+        new ApiKeyCredential(configuration["AzureOpenAI:ApiKey"])));
+
+builder.Build().Run();
